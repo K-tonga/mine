e0718934b1b1953b001e8ebb2d0fc394dae9a295
K-tonga <36051431+K-tonga@users.noreply.github.com>
2018-02-16 19:01:42 +0300

    Problem 2

---
+++ prob2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import glob
+import datetime
+import prob1
+
+def store_dataset(dataset):
+	'''Write the dataset to a text file, taking care not to overwrite
+	   any existing datasets
+	'''
+
+	timestamp = str(datetime.datetime.now())
+	timestamp = timestamp.replace("-", ".")
+	timestamp = timestamp.replace(":", ".")
+	timestamp = timestamp.replace(" ", ".")
+
+	suffix = ""
+
+	existing = glob.glob(timestamp + "*")
+	if (len(existing) > 0):
+		suffix = "_" + str(len(existing) + 1)
+	
+	timestamp += suffix;
+
+	with open(timestamp, "w") as out_f:
+
+		for key in sorted(dataset.keys(), key=int):
+
+			readings = ",".join(str(x) for x in dataset[key])
+
+			print(key + ":" + readings, file=out_f)
+
+	return 1
+
+
